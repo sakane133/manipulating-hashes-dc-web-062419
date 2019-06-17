62d0cb897b8ba63c87622a8696c70b62d0134dc8
Samantha Kane <s.kane133@gmail.com>
2019-06-17 03:12:51 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   contacts.each do |person, data|
     data.each do |attribute, value|
-      if attribute == favorite_icecream_flavors
+      if attribute == :favorite_icecream_flavors
         value.delete_if do |flavor|
          flavor  == "strawberry"
        end
